01f4d8c2b024c007a2a8f54ff1cd146847abd8b9
Sam Smith <samsmithnz@gmail.com>
2025-12-24 21:32:53 -0500

    added more tests

---
+++ src/SamSmithNZ.Tests/DataAccess/WorldCup/GoalDataAccessTests.cs
@@ -0,0 +1,150 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SamSmithnNZ.Tests;
+using SamSmithNZ.Service.DataAccess.WorldCup;
+using SamSmithNZ.Service.Models.WorldCup;
+using System.Collections.Generic;
+
+namespace SamSmithNZ.Tests.DataAccess.WorldCup
+{
+    [TestClass]
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    public class GoalDataAccessTests : BaseIntegrationTest
+    {
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_RegularGoal_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "45";
+            string playerName = "Neymar";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(45, results[0].GoalTime);
+            Assert.AreEqual(0, results[0].InjuryTime);
+            Assert.IsFalse(results[0].IsPenalty);
+            Assert.IsFalse(results[0].IsOwnGoal);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_PenaltyGoal_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "71(pen.)";
+            string playerName = "Neymar";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(71, results[0].GoalTime);
+            Assert.IsTrue(results[0].IsPenalty);
+            Assert.IsFalse(results[0].IsOwnGoal);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_OwnGoal_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "11(o.g.)";
+            string playerName = "Marcelo";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(11, results[0].GoalTime);
+            Assert.IsFalse(results[0].IsPenalty);
+            Assert.IsTrue(results[0].IsOwnGoal);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_InjuryTimeGoal_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "90+1";
+            string playerName = "Oscar";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(90, results[0].GoalTime);
+            Assert.AreEqual(1, results[0].InjuryTime);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_MultipleGoals_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "29, 71(pen.)";
+            string playerName = "Neymar";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(2, results.Count);
+            Assert.AreEqual(29, results[0].GoalTime);
+            Assert.IsFalse(results[0].IsPenalty);
+            Assert.AreEqual(71, results[1].GoalTime);
+            Assert.IsTrue(results[1].IsPenalty);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_EmptyString_ReturnsEmptyList()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "";
+            string playerName = "Test";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_NullString_ReturnsEmptyList()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = null;
+            string playerName = "Test";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(0, results.Count);
+        }
+
+        [TestMethod]
+        public void ProcessGoalHTMLMigration_WithHTMLEntities_ParsesCorrectly()
+        {
+            // Arrange
+            GoalDataAccess da = new(base.Configuration);
+            string goalText = "45&#39;";
+            string playerName = "Test";
+
+            // Act
+            List<Goal> results = da.ProcessGoalHTMLMigration(goalText, playerName);
+
+            // Assert
+            Assert.AreEqual(1, results.Count);
+            Assert.AreEqual(45, results[0].GoalTime);
+        }
+    }
+}
